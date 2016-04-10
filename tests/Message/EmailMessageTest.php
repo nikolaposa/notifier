776941a2d60ef010655d2634bfda0f35cadd538b
Nikola Posa <posa.nikola@gmail.com>
2016-04-10 11:56:30 +0200

    Testing Message implementations.

---
+++ tests/Message/EmailMessageTest.php
@@ -0,0 +1,54 @@
+<?php
+
+/*
+ * This file is part of the Notify package.
+ *
+ * Copyright (c) Nikola Posa <posa.nikola@gmail.com>
+ *
+ * For full copyright and license information, please refer to the LICENSE file,
+ * located at the package root folder.
+ */
+
+namespace Notify\Tests\Message;
+
+use PHPUnit_Framework_TestCase;
+use Notify\Message\EmailMessage;
+use Notify\Message\MessageInterface;
+use Notify\Message\HasSubjectInterface;
+use Notify\Message\HasSenderInterface;
+use Notify\Message\Actor\Recipients;
+use Notify\Message\Actor\Recipient;
+use Notify\Tests\TestAsset\Contact\TestContact;
+use Notify\Message\Content\TextContent;
+use Notify\Message\Content\ContentInterface;
+use Notify\Message\Actor\EmptySender;
+use Notify\Message\Actor\SenderInterface;
+use Notify\Message\Options\EmailOptions;
+
+/**
+ * @author Nikola Posa <posa.nikola@gmail.com>
+ */
+class EmailMessageTest extends PHPUnit_Framework_TestCase
+{
+    public function testEmailMessageImplementsAppropriateInterfaces()
+    {
+        $message = new EmailMessage(
+            new Recipients([
+                new Recipient('Test', new TestContact('test'))
+            ]),
+            'Test',
+            new TextContent('test test test'),
+            new EmptySender(),
+            new EmailOptions()
+        );
+
+        $this->assertInstanceOf(MessageInterface::class, $message);
+        $this->assertInstanceOf(HasSubjectInterface::class, $message);
+        $this->assertInstanceOf(HasSenderInterface::class, $message);
+        $this->assertInstanceOf(Recipients::class, $message->getRecipients());
+        $this->assertEquals('Test', $message->getSubject());
+        $this->assertInstanceOf(ContentInterface::class, $message->getContent());
+        $this->assertInstanceOf(SenderInterface::class, $message->getSender());
+        $this->assertInstanceOf(EmailOptions::class, $message->getOptions());
+    }
+}
