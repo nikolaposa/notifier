88b99625521e2459c60efec2f5dfca0c8e5a235c
Nikola Posa <posa.nikola@gmail.com>
2016-04-24 12:14:59 +0200

    Fixed SMS message.

--- src/Message/PushMessage.php
+++ src/Message/PushMessage.php
@@ -13,30 +13,22 @@ namespace Notify\Message;
 
 use Notify\Message\Actor\Recipients;
 use Notify\Message\Content\ContentProviderInterface;
-use Notify\Message\Actor\ActorInterface;
 
 /**
  * @author Nikola Posa <posa.nikola@gmail.com>
  */
-class SMSMessage extends AbstractMessage implements HasSenderInterface
+class PushMessage extends AbstractMessage
 {
-    const CONTENT_LENGTH_LIMIT = 160;
-
-    use HasSenderTrait;
-
     /**
      * @param Recipients $recipients
-     * @param string|ContentProviderInterface $content
+     * @param string|ContentProviderInterface|array $content
      * @param ActorInterface $sender
      */
     public function __construct(
         Recipients $recipients,
-        $content,
-        ActorInterface $sender = null
+        $content
     ) {
         parent::__construct($recipients, $content);
-
-        $this->sender = $sender;
     }
 
     /**
@@ -46,9 +38,11 @@ class SMSMessage extends AbstractMessage implements HasSenderInterface
     {
         $content = $this->loadContent();
 
-        return strlen($content) > self::CONTENT_LENGTH_LIMIT
-            ? substr($content, 0, self::CONTENT_LENGTH_LIMIT)
-            : $content;
+        if (is_array($content)) {
+            return json_encode($content);
+        }
+
+        return $content;
     }
 
     /**
@@ -56,6 +50,8 @@ class SMSMessage extends AbstractMessage implements HasSenderInterface
      */
     public function getRawContent()
     {
+        $this->loadContent();
+
         return $this->loadContent();
     }
 }
